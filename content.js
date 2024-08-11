9b54a561cf818a9ebc7e20ae44661fb72ab20d7b
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-08-11 17:08:43 -0400

    v0.0.1

--- content.js
+++ content.js
@@ -1,1 +1,26 @@
-alert("Its working")
+switch (window.location.hostname) {
+    case "www.youtube.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("YOUTUBE");
+      break;
+    case "www.facebook.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("FACEBOOK");
+      break;
+    case "www.netflix.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("NETFLIX");
+      break;
+    case "www.roblox.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("ROBLOX");
+      break;
+    case "discord.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("DISCORD");
+      break;
+    case "www.spotify.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("SPOTIFY");
+      break;
+}
