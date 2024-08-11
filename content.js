7e76902ce8c5960e7edc6282bf6f016d93019914
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-08-11 17:08:58 -0400

    v0.0.1

--- content.js
+++ content.js
@@ -1,3 +1,22 @@
+const generateHTML = (pageName) => {
+    return `
+     
+     <div id="clouds">
+        <div class="cloud x1"></div>
+        <div class="cloud x1_5"></div>
+        <div class="cloud x2"></div>
+        <div class="cloud x3"></div>
+        <div class="cloud x4"></div>
+        <div class="cloud x5"></div>
+    </div>
+    <div class='c'>
+        <div class='_404'>404</div>
+        <hr>
+        <div class='_1'>GET BACK TO WORK</div>
+        <div class='_2'>STUDYING > ${pageName}</div>
+    </div>
+     `;
+  };
 switch (window.location.hostname) {
     case "www.youtube.com":
       document.head.innerHTML = generateSTYLES();
